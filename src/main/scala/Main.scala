79d40887d3856195380e58c96cf43b438e30b88a
plee <pleelaprachakul@headstorn.com>
2020-03-18 11:17:22 -0400

    Add asserts for testing the result of endpoint request
    Add asserts for testing the result of endpoint request

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import RouteGenerator._
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+
 import sttp.tapir.docs.openapi._
 import sttp.tapir.openapi.OpenAPI
 import sttp.tapir.openapi.circe.yaml._
@@ -8,6 +9,13 @@ import sttp.tapir.swagger.akkahttp.SwaggerAkka
 
 import scala.concurrent.duration._
 import scala.concurrent.Await
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import sttp.tapir._
+import sttp.tapir.server.akkahttp._
+import sttp.client._
 
 object Main extends App {
 
@@ -38,7 +46,29 @@ object Main extends App {
     }
 
     val bindAndCheck = Http().bindAndHandle(routes, "localhost", 8080).map { _ =>
-        // testing
+        implicit val backend: SttpBackend[Identity, Nothing, NothingT] = HttpURLConnectionBackend()
+
+        //test hello GET endpoint
+        val result: String = basicRequest.response(asStringAlways).get(uri"http://localhost:8080/hello?name=Frodo").send().body
+//        println("Got result: " + result)
+        assert(result == "{\"code\":\"200\",\"msg\":\"hello : name : Frodo\"}")
+
+        //test bye POST endpoint
+        val result2: String = basicRequest.response(asStringAlways).post(uri"http://localhost:8080/bye?name=Frodo").send().body
+//        println("Got result: " + result2)
+        assert(result2 == "{\"code\":\"200\",\"msg\":\"bye : name : Frodo\"}")
+
+        //test streaming GET endpoint
+        val result3: String = basicRequest.response(asStringAlways).get(uri"http://localhost:8080/stream?text=Frodo").send().body
+//        println("Got result: " + result3)
+        assert(result3 == "{\n  \"code\" : \"200\",\n  \"msg\" : \"stream : text : Frodo\"\n}")
+
+        //test Not Found
+        val result4: String = basicRequest.response(asStringAlways).get(uri"http://localhost:8080/stream2?text=Frodo").send().statusText
+//        println("Got result: " + result4)
+        assert(result4 == "Not Found")
+
+        //running server
         println("Go to: http://localhost:8080/docs")
         println("Press any key to exit ...")
         scala.io.StdIn.readLine()
